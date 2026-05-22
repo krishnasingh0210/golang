f2cd1a197f2be2389a470be75d8674d57ad027f7
krishnasingh0210 <krishnasingh.02oct@gmail.com>
2026-05-22 22:09:02 +0530

    Second commit

--- project1/day1.go
+++ project1/day1.go
@@ -1,12 +1,23 @@
-package main
+package main // There can be only one main package in a Go program, and it must contain the main function, which is the entry point of the program.
 
 import "fmt"
 
-func main() {
-    var name string
-
-    fmt.Print("Enter name: ")
-    fmt.Scanln(&name)
-
-    fmt.Println("Hello", name)
+func add() {
+	var a, b int
+	fmt.Println("Enter two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("The numbers are:", a+b)
+}
+func string_reverse() {
+	var txt, rev string
+	fmt.Println("Enter a string:")
+	fmt.Scan(&txt)
+	for i := len(txt) - 1; i >= 0; i-- {
+		rev += string(txt[i])
+	}
+	fmt.Println("Reversed string:", rev)
+}
+func main(){
+    add()
+    string_reverse()
 }
